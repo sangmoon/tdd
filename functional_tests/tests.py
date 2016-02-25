2211561c84d68edefac3d10bc67b9158bdd5197f
sangmoon <cholsoo22001@gmail.com>
2016-02-25 22:25:22 +0900

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,14 @@
+"""dfafa fadfadf a.
+
+dfadsfads
+dfsdfadfa.
+"""
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
