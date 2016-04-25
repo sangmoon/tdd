36de9218d9c31f360310af28d029cb90a1d0eab7
parksangmoon <cholsoo22001@gmail.com>
2016-04-25 15:31:43 +0900

    Add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -7,7 +7,7 @@ REPO_URL = 'https://github.com/sangmoon/tdd.git'
 
 def deploy():
     site_folder = '/home/%s/sites/%s' % (env.user, env.host)
-    source_folder = site_folder + '/source'  # tdd 일껄 나한텐..
+    source_folder = site_folder + '/source'
     _create_directory_structure_if_necessary(site_folder)
     _get_latest_source(source_folder)
     _update_settings(source_folder, env.host)
@@ -18,7 +18,7 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
-        run('mkdir -p %s%s' % (site_folder, subfolder))
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 
 def _get_latest_source(source_folder):
@@ -26,22 +26,22 @@ def _get_latest_source(source_folder):
         run('cd %s && git fetch' % (source_folder,))
     else:
         run('git clone %s %s' % (REPO_URL, source_folder))
-    current_commit = local("git log -n 1 --format-%H", capture=True)
+    current_commit = local("git log -n 1 --format=%H", capture=True)
     run('cd %s && git reset --hard %s' % (source_folder, current_commit))
 
 
 def _update_settings(source_folder, site_name):
-    settings_path = source_folder + 'superlists/settings.py'
+    settings_path = source_folder + '/superlists/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
         'ALLOWED_HOSTS = .+$',
         'ALLOWED_HOSTS = ["%s"]' % (site_name,))
 
-    secret_key_file = source_folder + 'superlists/secret_key.py'
+    secret_key_file = source_folder + '/superlists/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "SECRETY_KEY = '%s'" % (key,))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
