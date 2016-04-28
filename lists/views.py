3bf36887d7249c4097fd9c901ba92b9b1761220a
parksangmoon <cholsoo22001@gmail.com>
2016-04-28 21:59:01 +0900

    rename all item input ids and names. still broken

--- lists/views.py
+++ lists/views.py
@@ -20,6 +20,7 @@ def view_list(request, list_id):
             return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
+            return render(request, 'home.html', {"error": error})
 
     return render(request, 'list.html', {'list': list_, 'error': error})
 
